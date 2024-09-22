638fef197b0b5efbe12fdb643b98c58b89b462ed
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2024-09-22 15:01:58 -0700

    Creacion del Proyecto

---
+++ CinepolisAgo-Dic2024/src/main/java/com/mycompany/cinepolisago/dic2024/CinepolisAgoDic2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cinepolisago.dic2024;
+
+/**
+ *
+ * @author Arturo ITSON
+ */
+public class CinepolisAgoDic2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
